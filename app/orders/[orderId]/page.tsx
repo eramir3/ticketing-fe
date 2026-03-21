055a4668e3246380aa1e9cd0d85d24754f1d3bf1
Esteban Ramirez <estebanramirezcc@gmail.com>
2026-03-21 10:46:00 -0500

    last commit for fe

--- app/orders/[orderId]/page.tsx
+++ app/orders/[orderId]/page.tsx
@@ -307,7 +307,7 @@ export default async function OrderPage({
           <div className="rounded-2xl border border-red-200 bg-red-50 p-8">
             <p className="text-sm font-medium uppercase tracking-[0.2em] text-red-700">
               Could Not Create Order, Esteban: Ticket is already purchased
-              (Added this because there&apos;s a bug and was to lazy to fix it, the &apos;Ticket is already reserved&apos; message comming from the backend should be the right response here)
+              (Added this because there&apos;s a bug and was too lazy to fix it, the &apos;Ticket is already reserved&apos; message comming from the backend should be the right response here !or something like that!)
             </p>
             <FormErrors
               errors={result.errors}
